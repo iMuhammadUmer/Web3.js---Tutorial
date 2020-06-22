7f8358cd2af4fe6d982136705e6b5e2c827f53bf
Muhammad Umer <mumer1297@gmail.com>
2020-06-22 21:57:17 +0500

    node modules updated

---
+++ Excercise_1/script.js
@@ -0,0 +1,15 @@
+console.log(Web3);
+const rpcURL = "HTTP://127.0.0.1:7545";
+let web3 = new Web3(rpcURL);
+
+let address = "0xe6001920C3a44f37781d3EdF656eaF15C4F612DB";
+
+web3.eth.getBalance(address, (err, wei) => {
+  if (err) {
+    alert("There is an error ", err);
+  } else {
+    alert("Wei ", wei);
+    let balance = web3.utils.fromWei(wei, "ether");
+    alert("Balance ", balance);
+  }
+});
