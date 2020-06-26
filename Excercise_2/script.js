f6feaee7d2d7849d011f762d7f87fbe9bab451d4
Muhammad Umer <mumer1297@gmail.com>
2020-06-26 07:16:01 +0500

    [feat] fetch account balance using infura RPC URL

--- Excercise_2/script.js
+++ Excercise_2/script.js
@@ -6,10 +6,10 @@ let address = "0x0f6fDCdfa8017003D6D868ADA9c691183F26D5Ff";
 
 web3.eth.getBalance(address, (err, wei) => {
   if (err) {
-    console.log("There is an error ", err);
+    alert("There is an error ", err);
   } else {
-    console.log("Wei ", wei);
+    alert("Wei ", wei);
     let balance = web3.utils.fromWei(wei, "ether");
-    console.log("Balance ", balance);
+    alert("Ether ", balance);
   }
 });
